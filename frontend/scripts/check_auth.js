d2c85270e2278a6805227be3eb068d186a7656cf
trussty42 <guschinandrey06@gmail.com>
2026-06-07 21:35:09 +0500

    upd

--- frontend/scripts/check_auth.js
+++ frontend/scripts/check_auth.js
@@ -1,25 +1,20 @@
 async function requireAuth() {
-
-    const token = localStorage.getItem('access');
+    const token = localStorage.getItem('ck_access_token');
 
     if (!token) {
         window.location.href = 'profile.html';
         return;
     }
 
-    const response = await fetch(
-        '/api/v1/users/me/',
-        {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
+    const response = await fetch('/api/v1/users/me/', {
+        headers: {
+            Authorization: `Bearer ${token}`
         }
-    );
+    });
 
     if (!response.ok) {
-
-        localStorage.removeItem('access');
-        localStorage.removeItem('refresh');
+        localStorage.removeItem('ck_access_token');
+        localStorage.removeItem('ck_refresh_token');
 
         window.location.href = 'profile.html';
     }
